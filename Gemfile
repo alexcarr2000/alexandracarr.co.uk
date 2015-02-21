6ab4d979fec55c968c96be80d76c5a94089d2905
Louis Pilfold <louis@lpil.uk>
2015-02-21 18:15:15 +0000

    Cannibalise my blog

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'middleman-core'
 gem 'middleman-sprockets', '~> 3.4.1'
-gem 'middleman-livereload', '~> 3.1.0'
+gem 'middleman-livereload', '~> 3.4.2'
+
+gem 'redcarpet', '~> 3.2.2'
 
 # gem 'middleman-s3_sync', '~> 3.0.41'
--- config.rb
+++ config.rb
@@ -1,72 +1,29 @@
-###
-# Compass
-###
+set :enviroment, :development
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+activate :livereload,
+         host: 'localhost',
+         apply_js_live: false,
+         apply_css_live: false,
+         no_swf: true
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+set :site_url, 'http://www.alexandracarr.co.uk'
+set :title, 'Alexandra Carr'
+set :description, 'Sculptor and Artist'
 
 set :css_dir, 'css'
-
 set :js_dir, 'js'
-
 set :images_dir, 'images'
+set :partials_dir, 'partials'
 
-# Build-specific configuration
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true,
+               smartypants: true
+
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
+  activate :minify_css
+  activate :minify_javascript
   activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  set :enviroment, :production
 end
+
+# page 'feed.xml', layout: false
