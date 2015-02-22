4a155604abaed2e4c79c3ff7cfbf6c9b5316d6bc
Louis Pilfold <louis@lpil.uk>
2015-02-22 00:27:08 +0000

    Social icons

---
+++ source/js/main.js
@@ -0,0 +1,5 @@
+(function() {
+  // Add CSS link for 'Awesome Font' icons
+  document.querySelector('.js-awesome-font-link').href =
+    '//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css';
+}());
